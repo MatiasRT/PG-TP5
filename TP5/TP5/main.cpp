0914992cb87ad57eed99c3e810bbaa5d9cde70e1
Matias Ruiz Torres <ruiztorresmatias@gmail.com>
2018-06-25 19:32:48 -0300

    TP5-V2
    Project now shows "highscore" ("nana" files are not included because they cant be uploaded to the repository)
    
    -Change "main" file.

--- TP5/TP5/main.cpp
+++ TP5/TP5/main.cpp
@@ -10,7 +10,7 @@ int main() {
 //MyLibScore myLibScore;
 Puntaje* p1 = new Puntaje;
 	p1->nombre = "Peter";
-	p1->puntos = 1000;
+	p1->puntos = 10000;
 	//myLibScore.RegistrandoPuntaje(p1);
 
 	Puntaje* p2 = new Puntaje;
@@ -23,19 +23,30 @@ Puntaje* p1 = new Puntaje;
 	p3->puntos = 6500;
 	//myLibScore.RegistrandoPuntaje(p3);
 
+	Puntaje* p4 = new Puntaje;
+	p4->nombre = "Frank";
+	p4->puntos = 2500;
+	//myLibScore.RegistrandoPuntaje(p3);
+
+	Puntaje* p5 = new Puntaje;
+	p5->nombre = "Jesse";
+	p5->puntos = 1500;
+	//myLibScore.RegistrandoPuntaje(p3);
+
 	//vector<Puntaje>vec1 = myLibScore.Highscore();
 
 	form fm;
 	listbox lb(fm, nana::rectangle(10, 10, 280, 180));
+
 	lb.append_header("Name", 150);
 	lb.append_header("Score", 120);
+
 	label lab{ fm, "<bold blue size=12>HighScore</>" };
 	lab.format(true);
 	button btn{ fm, "Quit" };
 	btn.events().click([&fm] {
 		fm.close();
 	});
-
 
 	/*for (vector<Puntaje>::iterator it = vec1.begin(); it != vec1.end(); ++it) {
 
@@ -45,10 +56,9 @@ Puntaje* p1 = new Puntaje;
 	lb.at(0).append({ (*p1).nombre, to_string(static_cast<int>((*p1).puntos)) });
 	lb.at(0).append({ (*p2).nombre, to_string(static_cast<int>((*p2).puntos)) });
 	lb.at(0).append({ (*p3).nombre, to_string(static_cast<int>((*p3).puntos)) });
+	lb.at(0).append({ (*p4).nombre, to_string(static_cast<int>((*p4).puntos)) });
+	lb.at(0).append({ (*p5).nombre, to_string(static_cast<int>((*p5).puntos)) });
 	
-	
-	
-
 	place plc(fm);
 	plc.div("<vert abc arrange=[20,130,20] gap=10>git");
 	plc.field("abc") << lab << lb << btn;	//These buttons are children of fm.
